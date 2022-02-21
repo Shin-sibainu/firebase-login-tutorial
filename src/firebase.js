afbb298d553421e78c4643ce4b2cd9ec73534e75
Shin-sibainu <shincode0712@gmail.com>
2022-02-21 15:24:07 +0900

    done

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhcmS7iWJyveyfo7lYepGK5etro5_VKKk",
-  authDomain: "auth-login-805f8.firebaseapp.com",
-  projectId: "auth-login-805f8",
-  storageBucket: "auth-login-805f8.appspot.com",
-  messagingSenderId: "789043990261",
-  appId: "1:789043990261:web:f5581f0e9e14b7adbbffbc",
+  /* apiKey等はあなたのアプリで設定されているものを挿入してください */
+  apiKey: "xxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
 };
 
 const app = initializeApp(firebaseConfig);
